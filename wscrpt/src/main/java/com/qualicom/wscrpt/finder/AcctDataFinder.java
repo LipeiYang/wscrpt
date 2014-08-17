1e11aa7e5a309ca7bfdcaefae8baee112d6851d7
leo.yang <lipei.yang@gmail.com>
2014-08-17 18:23:22 +0800

    catch exception

--- wscrpt/src/main/java/com/qualicom/wscrpt/finder/AcctDataFinder.java
+++ wscrpt/src/main/java/com/qualicom/wscrpt/finder/AcctDataFinder.java
@@ -9,7 +9,7 @@ import javax.persistence.Query;
 import org.apache.log4j.Logger;
 
 import com.qualicom.wscrpt.domain.AcctData;
-
+ 
 public class AcctDataFinder {
 	
     @SuppressWarnings("unchecked")
