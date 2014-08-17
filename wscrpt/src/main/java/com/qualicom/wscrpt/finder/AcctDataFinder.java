aafd13ed227bd289c9e7f143a50ee1a3cfda4717
leo.yang <lipei.yang@gmail.com>
2014-08-17 17:33:04 +0800

    catch exception in finder

--- wscrpt/src/main/java/com/qualicom/wscrpt/finder/AcctDataFinder.java
+++ wscrpt/src/main/java/com/qualicom/wscrpt/finder/AcctDataFinder.java
@@ -1,10 +1,12 @@
 package com.qualicom.wscrpt.finder;
 
-import java.util.Date;
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
 
 import com.qualicom.wscrpt.domain.AcctData;
 
@@ -12,17 +14,41 @@ public class AcctDataFinder {
 	
     @SuppressWarnings("unchecked")
 	public static List<AcctData> findAcctData(String suffix, int firstResult, int maxResults) {
-        EntityManager em = AcctData.entityManager();
-        Query q = em.createNativeQuery("select * from acct_data_"+suffix,AcctData.class);
-        return q.setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    	try
+    	{
+	        EntityManager em = AcctData.entityManager();
+	        Query q = em.createNativeQuery("select * from acct_data_"+suffix,AcctData.class);
+	        return q.setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    	}catch(Exception e)
+    	{
+    		Logger log = Logger.getLogger(AcctDataFinder.class);
+    		log.error("findAcctData error suffix:"+suffix+" firstResult:"+firstResult+" maxResults:"+maxResults);
+    		if(log.isDebugEnabled())
+    		{
+    			e.printStackTrace();
+    		}
+    		return Collections.EMPTY_LIST;
+    	}
     }
     
     public static AcctData findLastOne(String suffix, String acctUniqueId)
     {
-    	EntityManager em = AcctData.entityManager();
-    	Query q = em.createNativeQuery("select * from acct_data_"+suffix+" where acct_unique_id = :acctUniqueId order by tm_stmp desc", AcctData.class);
-    	q.setParameter("acctUniqueId", acctUniqueId);
-    	return (AcctData) q.getResultList().get(0);
+    	try {
+    		EntityManager em = AcctData.entityManager();
+    		Query q = em.createNativeQuery("select * from acct_data_"+suffix+" where acct_unique_id = :acctUniqueId order by tm_stmp desc", AcctData.class);
+    		q.setParameter("acctUniqueId", acctUniqueId);
+    		return (AcctData) q.getResultList().get(0);
+    	}
+    	catch(Exception e)
+    	{
+    		Logger log = Logger.getLogger(AcctDataFinder.class);
+    		log.error("findLastOne error suffix:"+suffix+" acctUniqueId:"+acctUniqueId);
+    		if(log.isDebugEnabled())
+    		{
+    			e.printStackTrace();
+    		}
+    		return null;
+    	}
     }
 
 }
