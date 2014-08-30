00df3a9382bc4f6522e225661f66c35ef7d81ada
leo.yang <lipei.yang@gmail.com>
2014-08-30 16:13:04 +0800

    change by leo for server b deploy

--- wscrpt/src/main/java/com/qualicom/wscrpt/utils/IntervalMapUtil.java
+++ wscrpt/src/main/java/com/qualicom/wscrpt/utils/IntervalMapUtil.java
@@ -10,35 +10,55 @@ import org.apache.log4j.Logger;
 public class IntervalMapUtil {
 	
 	private Map<String, Integer> intervalMap = new HashMap<String, Integer>(); 
-	private Integer defualtVlaue = 60; 
+	private Integer defualtValue = 60; 
+	private Integer maxValue = 60; 
 	
 	/*
 	 * return default value, if not found return "others"
 	 * */
 	
+	private Logger logger = Logger.getLogger(IntervalMapUtil.class);
+	
 	public IntervalMapUtil(ConfigFileReader intervalReader){
+		
 		Map<String,List<String>> intvalMap = intervalReader.getConfigMap();
 		for(String ssid : intvalMap.keySet()){
 			List<String> intervalStrList = intvalMap.get(ssid);
+			
+			if(null==intervalStrList||intervalStrList.size()==0)
+			{
+				logger.error("SSID ["+ssid+"] has no interval value.");
+				continue;
+			}
+			
 			int intvl_value = 0;
 			try {
 				intvl_value = Integer.valueOf(intervalStrList.get(intervalStrList.size() - 1));
-				if (intvl_value % 5 == 0)
-					intervalMap.put(ssid, intvl_value);
-				else
-					throw new NumberFormatException("Interval Map value not multiple of 5");
 			} catch (NumberFormatException e) {
-				Logger.getLogger(IntervalMapUtil.class.getName()).info(
-						"ssid " + ssid + " interval value [" + intvl_value
-								+ "]ignored ");
+				logger.error("SSID ["+ssid+"] interval value ["+intervalStrList.get(intervalStrList.size() - 1)+"] must be integer.");
+				continue;
 			}
+			
+			if(intvl_value % 5 != 0)
+			{
+				logger.error("SSID ["+ssid+"] interval value must be multiples of 5.");
+				continue;
+			}
+			
+			if (intvl_value > maxValue)
+			{
+				logger.error("SSID ["+ssid+"] interval value can not larger than "+maxValue+".");
+				continue;
+			}
+			
+			intervalMap.put(ssid, intvl_value);
 		}
 	}
 	
 	public Integer getInterval(String ssid)
 	{
 		if(intervalMap.get(ssid)==null)
-			return defualtVlaue;
+			return defualtValue;
 		return intervalMap.get(ssid);
 	}
 	
--- wscrpt/src/main/java/com/qualicom/wscrpt/utils/LinuxSpecialCharFilter.java
+++ wscrpt/src/main/java/com/qualicom/wscrpt/utils/LinuxSpecialCharFilter.java
@@ -2,6 +2,6 @@ package com.qualicom.wscrpt.utils;
 
 public class LinuxSpecialCharFilter {
 	public static String removeSpecChar(String s){
-		return s.replaceAll("[:<>&|/]", "");
+		return s.replaceAll("[:<>&|/?]", "");
 	}
 }
